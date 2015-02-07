597070f3b56e263f7df0cd7c8b2addd6a2014a41
ponyesteves <ajesteves@gmail.com>
2015-02-07 18:11:08 -0300

    correcion bug en lista de clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,8 +1,7 @@
 class Cliente < ActiveRecord::Base
 	def self.xUltimoUso (usuario)
 		find_by_sql("select * from clientes as a left join (select max(id) as gasto_id,cliente_id, 
-			user_id from gastos group by cliente_id) as b on a.id = b.cliente_id 
-			where user_id = #{usuario}
+			user_id from gastos where user_id = #{usuario} group by cliente_id) as b on a.id = b.cliente_id 
 			order by b.gasto_id desc")
 	end
 end
